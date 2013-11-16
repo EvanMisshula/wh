8b5016a16d7396cec9971c63cd6b18ee9da4a8f9
John Kelly Ferguson <hello@johnkellyferguson.com>
2013-11-16 13:59:08 -0500

    Add devise and coordinator model.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Wh::Application.routes.draw do
+  devise_for :coordinators
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
